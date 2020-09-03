377a17d96d11fc4d58217827dfe978723eb6af8b
Alexander Veselov <Alexander_Veselov@yahoo.com>
2020-09-04 01:36:44 +0300

    Add d3d12 stub files

---
+++ src/d3d12/d3d12_buffer.hpp
@@ -0,0 +1,15 @@
+#ifndef GPU_BUFFER_HPP_
+#define GPU_BUFFER_HPP_
+
+namespace gpu
+{
+    class Buffer
+    {
+    public:
+
+
+    };
+} // namespace gpu
+
+
+#endif // GPU_BUFFER_HPP_
---
+++ src/d3d12/d3d12_image.hpp
@@ -0,0 +1,13 @@
+#ifndef GPU_IMAGE_HPP_
+#define GPU_IMAGE_HPP_
+
+namespace gpu
+{
+    class Image
+    {
+    public:
+
+    };
+} // namespace gpu
+
+#endif // GPU_IMAGE_HPP_
